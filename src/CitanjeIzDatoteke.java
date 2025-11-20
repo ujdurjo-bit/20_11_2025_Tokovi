3f6eb929f79ae5cdd230cb1857361d525919e69c
Josip <ujdur.jo@gmail.com>
2025-11-20 21:01:57 +0100

    Kraj Dana

--- src/CitanjeIzDatoteke.java
+++ src/CitanjeIzDatoteke.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.*;
 
 public class CitanjeIzDatoteke {
+    public static void main(String[] args) throws IOException {
+        File ulazniFile = new File("dokument.txt");
+        File zapis = new File("kopija.txt");
+
+        FileReader ulaz = new FileReader(ulazniFile);
+        FileWriter izlaz = new FileWriter(zapis);
+        int c;
+        while ((c =ulaz.read()) != -1) {
+            izlaz.write(c);
+
+        }
+        ulaz.close();
+        izlaz.close();
+
+    }
 }
--- src/ZapisUDatoteku.java
+++ src/ZapisUDatoteku.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.io.*;
 
 public class ZapisUDatoteku {
+    public static void main(String[] args) throws IOException {
+        Reader ulaz = new StringReader("abcderf");
+        Writer izlaz = new FileWriter("izlaz.txt");
+        int nextChar;
+        while ((nextChar = ulaz.read()) != -1) {
+            izlaz.write(Character.toUpperCase(nextChar));
+
+        }
+//        izlaz.flush();
+        izlaz.close();
+
+
+    }
 }
